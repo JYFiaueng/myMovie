da237db91d63fd0d7cc6b0697ca9496c8addc71a
jyuf <jyuf@foxmail.com>
2017-02-06 19:28:26 +0800

    add

---
+++ js/main.js
@@ -0,0 +1,108 @@
+$(function (){
+	// 豆瓣的电影数据地址
+	var URL = 'https://api.douban.com/v2/movie/subject/';
+	// 全屏插件设置
+	$('#dowebok').fullpage({
+			sectionsColor: ['#fff', '#fff', '#fff'],
+			continuousVertical:true,
+			onLeave:function(index){
+				if(index === 3){
+					$('#poster').fadeOut(400, function (){
+						$('#poster').html('');
+					});
+					$('#info').fadeOut(400, function (){
+						$('#info').html('');
+					});
+					$('#describe').fadeOut(400, function (){
+						$('#describe').html('');
+					});
+				}
+			},
+			afterRender:function (){
+				$('#movieListP').css('height', $('html').height());
+				$('#showMovieP').css('height', $('html').height());
+				$('#poster').fadeOut();
+				$('#info').fadeOut();
+				$('#describe').fadeOut();
+			},
+	});
+	function throttle(method,context){//对函数进行节流
+		clearTimeout(method.tId);
+		method.tId=setTimeout(function(){
+				method.call(context);
+		},500);
+	}
+	$(window).resize(function(){
+		throttle(function (){
+			$('#movieListP').css('height', $('html').height());
+			$('#showMovieP').css('height', $('html').height());
+		}, window);
+	});
+	// 删除键盘和滚轮对全屏切换的控制
+	$.fn.fullpage.setKeyboardScrolling();
+	$.fn.fullpage.setAllowScrolling();
+	// 拿到电影类型数据
+	$.get('./data/type.json', function (data){
+		var html = '';
+		var w = 50;
+		for(var i = 0; i < data.length; i++){
+			html += '<div style="border-radius:'+w/2+'px;">'+data[i].Name+'</div>';
+		}
+		$('#typeList').html(html);
+	});
+	// 为电影类型添加点击事件
+	$('#typeList').on('click', 'div', function (e){
+		var typeName = e.target.innerHTML;
+		$.get('./data/'+typeName+'.json', function (data){
+			var html = '';
+			for(var i = 0; i < data.length; i++){
+				html += '<div data-id="'+data[i].id+'">'+data[i].title+'</div>';
+			}
+			$('#movieList').html(html);
+			$.fn.fullpage.moveSectionDown();
+		});
+	});
+	// 为电影名添加点击事件
+	$('#movieList').on('click', 'div', function (e){
+		var movieId = $(e.target).attr('data-id');
+		$.ajax({
+			url:URL + movieId,
+			cache:true,
+			type:'get',
+			dataType:'jsonp',
+			crossDomain:true,
+			jsonp:'callback',
+			success:function (data){
+				$('#load').fadeOut(0);
+				$('#poster').fadeIn(400);
+				$('#info').fadeIn(400);
+				$('#describe').fadeIn(400);
+				$('#poster').html('<img class="img-responsive" src="'+data.images.large+'"/>');
+				var html = '';
+				html += '<dt>名字 :</dt><dd>&nbsp;&nbsp;'+data.title+'</dd>';
+				html += '<dt>原名 :</dt><dd>&nbsp;&nbsp;'+data.current_title+'</dd>';
+				html += '<dt>评分 :</dt><dd>&nbsp;&nbsp;'+data.rating.average+'</dd>';
+				html += '<dt>导演 :</dt><dd>&nbsp;&nbsp;'+data.directors[0].name+'</dd>';
+				html += '<dt>主演 :</dt><dd>&nbsp;&nbsp;'+data.casts[0].name+'</dd>';
+				html += '<dt>年份 :</dt><dd>&nbsp;&nbsp;'+data.year+'</dd>';
+				html += '<dt>豆瓣 :</dt><dd>&nbsp;&nbsp;<a target="blank" href="'+data.alt+'">点这里</a></dd>';
+				html += '<dt>国家 :</dt><dd>&nbsp;&nbsp;'+data.countries.join('/')+'</dd>';
+				html += '<dt>类型 :</dt><dd>&nbsp;&nbsp;'+data.genres.join('/')+'</dd>';
+				$('#info').html(html);
+				$('#describe').html(data.summary.split('\n').join('<br/>'));
+			}
+		});
+		$('#load').fadeIn(0);
+		$.fn.fullpage.moveSectionDown();
+	});
+	// 按钮事件
+	$('#MoviesGoTypes').click(function (){
+		$.fn.fullpage.moveSectionUp();
+	});
+	$('#MovieGoMovies').click(function (){
+		$.fn.fullpage.moveSectionUp();
+	});
+	$('#MovieGoTypes').click(function (){
+		$.fn.fullpage.moveSectionDown();
+	});
+});
