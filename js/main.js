bbd37ae5795f91e9fa820c8c2f29d7f0a9aa8897
jiayufeng <jiayufeng@innobuddy.com>
2017-10-15 11:52:29 +0800

    add audio

--- js/main.js
+++ js/main.js
@@ -9,8 +9,18 @@ $(function (){
 	var typeList = $('#typeList');
 	var movieList = $('#movieList');
 	var load = $('#load');
+	var audio = './data/Bandari.mp3';
 	// 豆瓣的电影数据地址
 	var URL = 'https://api.douban.com/v2/movie/subject/';
+	function exploedAudio(src){
+		var audio = document.createElement('audio');
+		audio.src = src;
+		audio.onload = function () {
+			console.log('over');
+		};
+		audio.autoplay = true;
+	}
+	exploedAudio(audio);
 	// 全屏插件设置
 	$('#dowebok').fullpage({
 			sectionsColor: ['#f4f4f4', '#f2f2f2', '#efefef'],
